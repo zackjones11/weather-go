1954dcf3f0d7ea1f4f701a0cd4a1d2bcd8cbddc8
Zack Jones <zackjones11@gmail.com>
2021-02-06 20:30:31 +0100

    Response and Weather struct

--- pkg/weather/api.go
+++ pkg/weather/api.go
@@ -10,6 +10,17 @@ type Client struct {
 	key  string
 }
 
+// Response contains the whole response from the API
+type Response struct {
+	Weather []Weather `json:"weather"`
+}
+
+// Weather contains the information for the weather requested
+type Weather struct {
+	Main        string `json:"main"`
+	Description string `json:"description"`
+}
+
 // NewClient creates and returns a new Client instance for making requests to the Weather API
 func NewClient(httpClient *http.Client, key string) *Client {
 	return &Client{httpClient, key}
