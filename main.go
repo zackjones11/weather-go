9262abcacacc0dae5e0f462301166f8427f2c685
Zack Jones <zackjones11@gmail.com>
2021-02-07 20:16:22 +0100

    create a newClient for photo api

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/zackjones11/weather-go/pkg/photo"
 	"github.com/zackjones11/weather-go/pkg/weather"
 )
 
@@ -16,18 +17,26 @@ func main() {
 		log.Println("Error: Cannot load .env file")
 	}
 
-	apiKey := os.Getenv("WEATHER_API_KEY")
-	if apiKey == "" {
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	weatherAPIKey := os.Getenv("WEATHER_API_KEY")
+	if weatherAPIKey == "" {
 		log.Fatal("Error: Please add a WEATHER_API_KEY to .env")
 	}
 
-	client := &http.Client{Timeout: 10 * time.Second}
-	weatherAPI := weather.NewClient(client, apiKey)
+	weatherAPI := weather.NewClient(client, weatherAPIKey)
+
+	photoAPIKey := os.Getenv("PHOTO_API_KEY")
+	if photoAPIKey == "" {
+		log.Fatal("Error: Please add a PHOTO_API_KEY to .env")
+	}
+
+	photoAPI := photo.NewClient(client, photoAPIKey)
 
 	assetsFs := http.FileServer(http.Dir("public/assets"))
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", assetsFs))
 	mux.HandleFunc("/", weather.SearchHandler)
-	mux.HandleFunc("/weather", weather.DetailHandler(weatherAPI))
+	mux.HandleFunc("/weather", weather.DetailHandler(photoAPI, weatherAPI))
 	http.ListenAndServe(":8080", mux)
 }
